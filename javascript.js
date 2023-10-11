03d5fef09f0b947635014f75492a74d186efb6aa
Lspacedev <145987706+Lspacedev@users.noreply.github.com>
2023-10-11 11:33:02 +0200

    Improved game logic, results display in DOM instead of console log, added score board

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+
 function getComputerChoice(){
     let min = 0;
     let max = 3;
@@ -8,41 +9,85 @@ function getComputerChoice(){
     return computerChoice;
 }
 
+//A single game round
+//let scorePlayer = 0;
+ //let scoreComputer = 0;
+let i = 0;
+let x = 0;
+
+let results = document.querySelector('#results');
+let playerScore = document.querySelector('.playerScore');
+let computerScore = document.querySelector('.computerScore');
 
 function round(playerSelection, computerSelection){
-    let player = playerSelection.toLowerCase();
-    let computer = computerSelection.toLowerCase();
+    let player = playerSelection;
+    let computer = computerSelection;
 
     if(player === 'rock' && computer === 'rock'){
-        return "Tie!";
+        results.textContent = "Tie!";
+
     }else if(player === 'paper' && computer === 'paper'){
-        return "Tie!";
-    }else if(player === 'rock' && computer === 'rock'){
-        return "Tie!";
+        results.textContent = "Tie!";
+
+    }else if(player === 'scissors' && computer === 'scissors'){
+        results.textContent = "Tie!";
+
     }else if(player === 'rock' && computer === 'scissors'){
-        return "You Win! Rock beats Scissors";
+        i = i + 1;
+        results.textContent = "You Win! Rock beats Scissors";
+        playerScore.textContent = i;
+        
+
     }else if(player === 'paper' && computer === 'rock'){
-        return "You Win! Paper beats Rock";
+        i = i + 1;
+        results.textContent = "You Win! Paper beats Rock";
+        playerScore.textContent = i;
+        
     }else if(player === 'scissors' && computer === 'paper'){
-        return "You Win! Scissors beats paper";
+        i = i + 1;
+        results.textContent = "You Win! Scissors beats paper";
+        playerScore.textContent = i;
+        
+
     }else if(player === 'rock' && computer === 'paper'){
-        return "You Lose! Paper beats Rock";
+        x = x + 1;
+        results.textContent = "You Lose! Paper beats Rock";
+        computerScore.textContent = x;
+       
+
     }else if(player === 'scissors' && computer === 'rock'){
-        return "You Lose! Rock beats Scissors";
+        x = x + 1;
+        results.textContent = "You Lose! Rock beats Scissors";
+        computerScore.textContent = x;
+        
+
     }else if(player === 'paper' && computer === 'scissors'){
-        return "You Lose! Scissors beats Paper";
+        x = x + 1;
+        results.textContent = "You Lose! Scissors beats Paper";
+        computerScore.textContent = x;
+
     }
-
+    
 }
 
-function game(){
-    for(let i = 0; i < 5; i++){
-        let playerChoice = prompt("Choose Rock, Paper or Scissors");
-        let compChoice = getComputerChoice();
-        console.log(round(playerChoice, compChoice));
-    }   
 
-  
-}
+const buttons = document.querySelectorAll('.buttons > button');
 
-game();
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+    if( i === 5){
+        results.textContent = "You beat the computer";
+    }else if(x === 5){
+        results.textContent = "You lost to the computer";
+    }else{
+        playerChoice = button.id;
+        
+        let compChoice = getComputerChoice().toLowerCase();
+
+        round(playerChoice, compChoice);}
+    });
+    
+});
+
+
+
